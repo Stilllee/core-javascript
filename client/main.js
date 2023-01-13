d76648f99f6b3b72501aaf216ce4714b0de45f99
Stilllee <orodae@gmail.com>
2023-01-14 01:46:54 +0900

    1/13

--- client/main.js
+++ client/main.js
@@ -1,73 +1,96 @@
-//1. 인풋 밸류값 가져오기
-//2. 이벤트 핸들러 연결하기
-//3. 이벤트 기본동작 차단하기
-//4. 두 수의 합을 더해주기
-//5. 화면에 출력하기
 
-/* 
-// 내코드
-let firstNumber = getNode('#firstNumber');
-let secondNumber = getNode('#secondNumber');
-let done = getNode('#done');
-let result = getNode('.result');
 
-let calculator = (e) => {
-  let num1 = Number(firstNumber.value);
-  let num2 = Number(secondNumber.value);
-  let num3 = num1 + num2;
-  e.preventDefault();
-  result.textContent = num3;
-};
 
-done.addEventListener('click', calculator); */
+/* eslint no-undef:'warn' */
+/* eslint no-unused-vars: 'off' */
 
-// 범쌤코드
+
+
+
+
+
+
+// 1. 인풋 벨류값 가져오기
+// 2. 이벤트 핸들러 연결하기
+// 3. 이벤트 기본동작 차단하기
+// 4. 두 수의 합을 더해주기 
+// 5. 화면에 출력하기
+
+
+import { 
+  getNode,
+  sum,
+  getInputValue,
+  clearContents,
+  insertLast,
+ } from "./lib/index.js";
+
+
+
+ 
+
+
+
 const firstInput = getNode('#firstNumber');
 const secondInput = getNode('#secondNumber');
 const done = getNode('#done');
 const result = getNode('.result');
 
-function getInputValue(node) {
-  if (typeof node === 'string') node = getNode(node);
-  if (node.tagName !== 'INPUT')
-    refError('getInputValue 함수는 INPUT ELEMENT만 허용합니다.');
-  return node.value;
-}
 
-// const sum = (valueA,valueB) => valueA + valueB;
 
-function sum(valueA, valueB) {
-  return valueA + valueB;
-}
 
-function clearContents(node) {
-  if (typeof node === 'string') node = getNode(node);
-  node.textContent = '';
-}
-
-function handler(e) {
+function handler(e){
   e.preventDefault();
 
   let firstValue = +getInputValue(firstInput);
   let secondValue = +getInputValue(secondInput);
-  let total = sum(firstValue, secondValue);
+  let total = sum(firstValue, secondValue)
 
+
+  
   clearContents(result);
 
-  insertLast(result, total);
+  insertLast(result,total);
+  
 }
 
-function inputHandler() {
+
+
+function inputHandler(){
   let firstValue = +getInputValue(firstInput);
   let secondValue = +getInputValue(secondInput);
-  let total = sum(firstValue, secondValue);
+  let total = sum(firstValue, secondValue)
 
+
+  
   clearContents(result);
 
-  insertLast(result, total);
+  insertLast(result,total);
 }
 
-done.addEventListener('click', handler);
 
-firstInput.addEventListener('change', inputHandler);
-secondInput.addEventListener('change', inputHandler);
+
+done.addEventListener('click',handler)
+
+firstInput.addEventListener('change',inputHandler)
+secondInput.addEventListener('change',inputHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
