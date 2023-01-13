9078cafc235c5335833b6e671dd589b7862541e4
Stilllee <orodae@gmail.com>
2023-01-14 01:29:47 +0900

    1/13

--- client/main.js
+++ client/main.js
@@ -1,73 +1,39 @@
-//1. 인풋 밸류값 가져오기
-//2. 이벤트 핸들러 연결하기
-//3. 이벤트 기본동작 차단하기
-//4. 두 수의 합을 더해주기
-//5. 화면에 출력하기
+import {
+  clearContents,
+  getInputValue,
+  getNode,
+  getRandom,
+  insertLast,
+  isNumericString,
+  addClass,
+  removeClass,
+} from './lib/index.js';
 
-/* 
-// 내코드
-let firstNumber = getNode('#firstNumber');
-let secondNumber = getNode('#secondNumber');
-let done = getNode('#done');
-let result = getNode('.result');
+import { jujeobData } from './data/data.js';
 
-let calculator = (e) => {
-  let num1 = Number(firstNumber.value);
-  let num2 = Number(secondNumber.value);
-  let num3 = num1 + num2;
-  e.preventDefault();
-  result.textContent = num3;
-};
+const submit = getNode('#submit');
+const resultArea = getNode('.result');
 
-done.addEventListener('click', calculator); */
-
-// 범쌤코드
-const firstInput = getNode('#firstNumber');
-const secondInput = getNode('#secondNumber');
-const done = getNode('#done');
-const result = getNode('.result');
-
-function getInputValue(node) {
-  if (typeof node === 'string') node = getNode(node);
-  if (node.tagName !== 'INPUT')
-    refError('getInputValue 함수는 INPUT ELEMENT만 허용합니다.');
-  return node.value;
-}
-
-// const sum = (valueA,valueB) => valueA + valueB;
-
-function sum(valueA, valueB) {
-  return valueA + valueB;
-}
-
-function clearContents(node) {
-  if (typeof node === 'string') node = getNode(node);
-  node.textContent = '';
-}
-
-function handler(e) {
+function clickSubmitHandler(e) {
   e.preventDefault();
 
-  let firstValue = +getInputValue(firstInput);
-  let secondValue = +getInputValue(secondInput);
-  let total = sum(firstValue, secondValue);
+  let name = getInputValue('#nameField');
+  let list = jujeobData(name);
+  let pick = list[getRandom(list.length - 1)];
 
-  clearContents(result);
+  if (!name) {
+    console.log('이름을 입력해 주세요!');
+    showAlert('.alert-error', '잘못된 정보입니다.!', 2000);
+    return;
+  }
 
-  insertLast(result, total);
+  if (isNumericString(name)) {
+    console.log('제대로된 이름을 입력해주세요.');
+    return;
+  }
+
+  clearContents(resultArea);
+  insertLast(resultArea, pick);
 }
 
-function inputHandler() {
-  let firstValue = +getInputValue(firstInput);
-  let secondValue = +getInputValue(secondInput);
-  let total = sum(firstValue, secondValue);
-
-  clearContents(result);
-
-  insertLast(result, total);
-}
-
-done.addEventListener('click', handler);
-
-firstInput.addEventListener('change', inputHandler);
-secondInput.addEventListener('change', inputHandler);
+submit.addEventListener('click', clickSubmitHandler);
